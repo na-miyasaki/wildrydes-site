248bc29eabb4032a92736dfe0993c4613c68866a
na-miyasaki <132875558+na-miyasaki@users.noreply.github.com>
2023-05-12 15:21:58 +0900

    updated title

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ap-southeast-2_cWIrpd8TI', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5a6ur4su8lg38b9c53r20mj1n8', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-southeast-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
